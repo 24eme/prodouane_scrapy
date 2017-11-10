2e35dde1c8753452e21e1355385d4b7db63b07a2
Vincent LAURENT <vince.laurent@gmail.com>
2017-11-10 12:16:08 +0100

    Numéro CVI dans l'export des drev

--- phantomjs_scrapping/scripts/drevCVI.sh
+++
@@ -1,3 +0,0 @@
-
-echo "id;cvi" > data/drevCVI.csv
-rgrep -nE "<td>[0-9]+</td><td>[0-9]{4}</td>" html/breforedrev_* | sed 's/<td>//g' | sed 's|</td>|;|g' | sed -r 's/[ \t]*//g' | sed 's/;$//' | sed 's|html/breforedrev_||' | sed -r 's/\.html:[0-9]+:/;/' | grep ";2016$" >> data/drevCVI.csv
