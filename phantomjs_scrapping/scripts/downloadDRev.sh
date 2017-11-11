eafb03f7b4f1b86cca844e6b25856bb4112c5881
Tangui Morlier <tangui@tangui.eu.org>
2017-11-11 16:18:45 +0100

    Récupère les valeurs de la conf

--- phantomjs_scrapping/scripts/downloadDRev.sh
+++ phantomjs_scrapping/scripts/downloadDRev.sh
@@ -7,6 +7,6 @@ awk -F ';' '{print $1}'  data/ppm.csv data/cvi.csv  | sort -u > /tmp/cvi_ppm.lis
 cat /tmp/cvi_ppm.list | while read id ; do
   mycookie=$(QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/login.js $domain $utilisateur_drev $motdepasse_drev | sed 's/ASP.NET_SessionId=//' ) ;
   echo $mycookie
-  QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/drev.js extraviti.com $id $mycookie ;
+  QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/drev.js $domain $id $mycookie ;
   QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/exit.js $domain $mycookie
 done
