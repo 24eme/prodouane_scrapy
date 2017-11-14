7cbd1e9f95730b358fda9ef72030a0df67fad73f
Tangui Morlier <tangui@tangui.eu.org>
2017-11-14 12:32:58 +0100

    Permet le téléchargement unitaire de DRev

--- phantomjs_scrapping/scripts/downloadDRev.sh
+++ phantomjs_scrapping/scripts/downloadDRev.sh
@@ -2,11 +2,17 @@
 
 . scripts/config.inc
 
-awk -F ';' '{print $1}'  data/ppm.csv data/cvi.csv  | sort -u > /tmp/cvi_ppm.list
+if ! test $1 ; then
+	awk -F ';' '{print $1}'  data/ppm.csv data/cvi.csv  | sort -u > /tmp/cvi_ppm_$$.list
+else
+	echo $* | sed 's/ /\n/g' | grep '.' > /tmp/cvi_ppm_$$.list
+fi
 
-cat /tmp/cvi_ppm.list | while read id ; do
+cat /tmp/cvi_ppm_$$.list | while read id ; do
   mycookie=$(QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/login.js $domain $utilisateur_drev $motdepasse_drev | sed 's/ASP.NET_SessionId=//' ) ;
   echo $mycookie
   QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/drev.js $domain $id $mycookie ;
   QT_QPA_FONTDIR=/usr/share/fonts QT_QPA_PLATFORM=offscreen phantomjs scripts/exit.js $domain $mycookie
 done
+
+rm /tmp/cvi_ppm_$$.list
