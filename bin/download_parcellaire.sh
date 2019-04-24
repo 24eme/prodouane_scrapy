5e6260ee54191c823d40ba19ca9ae3e973f1b5ea
Gabriel Poma <gpoma@24eme.fr>
2019-04-24 12:04:53 +0200

    Incrementation code d'erreur

--- bin/download_parcellaire.sh
+++ bin/download_parcellaire.sh
@@ -7,7 +7,7 @@ fi
 
 if [ $# -ne 1 ]; then
 	echo "Il manque un paramètre"
-	exit 1
+	exit 2
 fi
 
 cvi=$1
@@ -17,14 +17,14 @@ cd $(dirname $0)/../ > /dev/null 2>&1
 
 if ! test "$PRODOUANE_USER" || ! test "$PRODOUANE_PASS"; then
 	echo "Authentification mal configurée dans le fichier de config"
-	exit 1
+	exit 3
 fi
 
 CVI="$cvi" PRODOUANE_USER="$PRODOUANE_USER" PRODOUANE_PASS="$PRODOUANE_PASS" scrapy crawl parcellaire
 
 if [ ! -f "./documents/parcellaire-${cvi}-parcellaire.html" ]; then
 	echo "Échec du scraping"
-	exit 2
+	exit 4
 fi
 
 python posttraitement/parcellaire_to_csv.py $cvi
