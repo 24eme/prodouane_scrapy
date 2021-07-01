af89f55945d7bff6569b718b1fb5a9d92f71729f
Vincent LAURENT <vince.laurent@gmail.com>
2021-07-01 22:24:17 +0200

    Test que le fichier est non vide dans le debug

--- bin/test_debug.sh
+++ bin/test_debug.sh
@@ -19,9 +19,9 @@ echo "================"
 rm -f "documents/dr-2019-"$PRODOUANE_DEBUG_DR""*
 PRODOUANE_DEBUG=true bash bin/download_douane.sh dr 2019 $PRODOUANE_DEBUG_DR > debug/dr-2019.log 2>&1
 
-if test -f documents/dr-2019-"$PRODOUANE_DEBUG_DR".pdf ;  then echo " PDF Présent :)";  else 	echo "Erreur pas de pdf dr";  fi
-if test -f documents/dr-2019-"$PRODOUANE_DEBUG_DR".html ; then echo " HTML Présent :)"; else    echo "Erreur pas de HTML dr"; fi
-if test -f documents/dr-2019-"$PRODOUANE_DEBUG_DR".xls ;  then echo " XLS Présent :)";  else    echo "Erreur pas de xls dr";  fi
+if test -s documents/dr-2019-"$PRODOUANE_DEBUG_DR".pdf ;  then echo " PDF Présent :)";  else 	echo "Erreur pas de pdf dr";  fi
+if test -s documents/dr-2019-"$PRODOUANE_DEBUG_DR".html ; then echo " HTML Présent :)"; else    echo "Erreur pas de HTML dr"; fi
+if test -s documents/dr-2019-"$PRODOUANE_DEBUG_DR".xls ;  then echo " XLS Présent :)";  else    echo "Erreur pas de xls dr";  fi
 
 echo
 echo "test SV11"
@@ -29,9 +29,9 @@ echo "================"
 rm -f "documents/sv11-2019-"$PRODOUANE_DEBUG_SV11""*
 PRODOUANE_DEBUG=true bash bin/download_douane.sh sv11 2019 $PRODOUANE_DEBUG_SV11 > debug/sv11-2019.log 2>&1
 
-if test -f documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".pdf ;  then echo " PDF Présent :)" ;  else echo "Erreur pas de pdf sv11";  fi
-if test -f documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".html ; then echo " HTML Présent :)" ; else echo "Erreur pas de html sv11"; fi
-if test -f documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".xls ;  then echo " XLS Présent :)" ;  else echo "Erreur pas de xls sv11";  fi
+if test -s documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".pdf ;  then echo " PDF Présent :)" ;  else echo "Erreur pas de pdf sv11";  fi
+if test -s documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".html ; then echo " HTML Présent :)" ; else echo "Erreur pas de html sv11"; fi
+if test -s documents/sv11-2019-"$PRODOUANE_DEBUG_SV11".xls ;  then echo " XLS Présent :)" ;  else echo "Erreur pas de xls sv11";  fi
 
 echo
 echo "test SV12"
@@ -39,9 +39,9 @@ echo "================"
 rm -f "documents/sv12-2019-"$PRODOUANE_DEBUG_SV12""*
 PRODOUANE_DEBUG=true bash bin/download_douane.sh sv12 2019 $PRODOUANE_DEBUG_SV12 > debug/sv12-2019.log 2>&1
 
-if test -f documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".pdf  ; then echo " PDF Présent :)";  else echo "Erreur pas de pdf dr";  fi
-if test -f documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".html ; then echo " HTML Présent :)"; else echo "Erreur pas de html dr"; fi
-if test -f documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".xls  ; then echo " XLS Présent :)";  else echo "Erreur pas de xls dr";  fi
+if test -s documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".pdf  ; then echo " PDF Présent :)";  else echo "Erreur pas de pdf dr";  fi
+if test -s documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".html ; then echo " HTML Présent :)"; else echo "Erreur pas de html dr"; fi
+if test -s documents/sv12-2019-"$PRODOUANE_DEBUG_SV12".xls  ; then echo " XLS Présent :)";  else echo "Erreur pas de xls dr";  fi
 
 echo
 echo "test Parcellaire"
@@ -49,10 +49,10 @@ echo "================"
 rm -f "documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE""*
 PRODOUANE_DEBUG=true bash bin/download_parcellaire.sh $PRODOUANE_DEBUG_PARCELLAIRE > debug/parcellaire.log 2>&1
 
-if test -f documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-parcellaire.html ; then echo " HTML Parcellaire Présent :)"; else echo "Erreur pas de HTML parcellaire"; fi
-if test -f documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-accueil.html ; then echo " HTML accueil Parcellaire Présent :)"; else echo "Erreur pas de HTML accueil Parcellaire"; fi
-if test -f documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-parcellaire.pdf ; then echo " PDF Parcellaire Présent :)"; else echo "Erreur pas de PDF Parcellaire"; fi
-if test -f documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE".csv ; then echo " CSV Parcellaire Présent :)"; else echo "Erreur pas de CSV Parcellaire"; fi
+if test -s documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-parcellaire.html ; then echo " HTML Parcellaire Présent :)"; else echo "Erreur pas de HTML parcellaire"; fi
+if test -s documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-accueil.html ; then echo " HTML accueil Parcellaire Présent :)"; else echo "Erreur pas de HTML accueil Parcellaire"; fi
+if test -s documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE"-parcellaire.pdf ; then echo " PDF Parcellaire Présent :)"; else echo "Erreur pas de PDF Parcellaire"; fi
+if test -s documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE".csv ; then echo " CSV Parcellaire Présent :)"; else echo "Erreur pas de CSV Parcellaire"; fi
 
 
 
