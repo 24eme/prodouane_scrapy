005fca004823b27bd9cbd2a456930039d2269553
Vincent LAURENT <vince.laurent@gmail.com>
2021-07-02 10:33:51 +0200

    Meilleur nettoyage des fichiers pour un meilleur debug

--- bin/test_debug.sh
+++ bin/test_debug.sh
@@ -59,7 +59,7 @@ if test -s documents/parcellaire-"$PRODOUANE_DEBUG_PARCELLAIRE".csv ; then echo 
 sed -i 's/value="[^"]*="/value="CLEANED"/g' debug/*
 sed -i 's/value="[0-9-][0-9]*:[0-9-]*"/value="CLEANED"/g' debug/*
 sed -i 's/CDATA\[[0-9-][0-9]*:[0-9-]*\]/CDATA[CLEANED]/g' debug/*
-sed -i 's/token=[^">]*\([">]\)/token=CLEANED\1/g' debug/*
+sed -i -r 's/token="[^">]*([">])/token="CLEANED\1/g' debug/*
 sed -i 's/views*_dom_id[":]*[^"]*"/view_dom_id:CLEANED/g' debug/*
 sed -i 's/RichFaces.panelTabs.*//' debug/*
 sed -i 's/permissionsHash":"[^"]*"/permissionsHash":"CLEANED"/' debug/*
@@ -67,6 +67,7 @@ sed -i 's/javax.faces.ViewState" value="[^"]*"/javax.faces.ViewState" value="CLE
 sed -i 's/javax.faces.ViewState" value="[^"]*"/javax.faces.ViewState" value="CLEANED"/' debug/*
 sed -i 's/input type="hidden" name="token" value="[0-9_]*"/input type="hidden" name="token" value="CLEANED"/' debug/*
 sed -i 's/SAMLResponse" value="[^"]*"/SAMLResponse" value="CLEANED"/' debug/*
+sed -i -r 's/(class="localeDate" val=")[0-9]+(")/\1CLEANED\2/' debug/*
 
 cd debug
 
