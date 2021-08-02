47489a4ef50438d36e5be1fdca651ecda9fea08d
Tangui Morlier <tangui@tangui.eu.org>
2021-08-02 17:39:27 +0200

    Récupère les délimitations de communes

---
+++ bin/get_delimination_aoc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+cd $(dirname $0)/../
+
+PRODOUANE_NO_WWWDATA=true
+
+if ! which ogr2ogr > /dev/null; then
+    echo "ogr2ogr missing (sudo apt install gdal-bin)"
+    exit 3
+fi
+
+. bin/common.inc
+
+rm -rf geo/*
+mkdir -p geo/features
+cd geo
+curl -s -L https://www.data.gouv.fr/fr/datasets/r/e79a7c68-2fe4-4225-a802-8379a8d6426c > e79a7c68-2fe4-4225-a802-8379a8d6426c.zip
+unzip -q e79a7c68-2fe4-4225-a802-8379a8d6426c.zip 
+ogr2ogr -f GeoJSON -t_srs crs:84 output.geojson *delim_parcellaire_aoc_shp.shp
+cat output.geojson | jq --compact-output ".features[]" | split -l 1 --additional-suffix=".geojson" /dev/stdin "features/"$i
+cd ..
+rm -rf communes
+mkdir -p communes
+rgrep -l '"id_app":'$INAO_ID_APP',' geo/features/ | while read json ; do
+    insee=$(jq .properties.insee $json | sed 's/"//g')
+    echo '{"type": "FeatureCollection","name": "aoc_geojson","crs": {"type": "name","properties": {"name": "urn:ogc:def:crs:EPSG::2154"}},"features": [' > "communes/delimitation-"$insee".json"
+    cat $json >> "communes/delimitation-"$insee".json"
+    echo ']}' >> "communes/delimitation-"$insee".json"
+done
