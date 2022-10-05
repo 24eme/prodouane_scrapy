6b53deabede99c3b506932bca08fd48dbc5ba229
Honorine-MC <honorine.mariechristy@gmail.com>
2022-10-05 15:32:03 +0200

    script pour télécharger 2 pages html et le pdf pour un CVI donné depuis le site de la douane

---
+++ puppeteer_scrapping/scriptGetHTMLSAndPDFByCVI.js
@@ -0,0 +1,132 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+(async () => {
+  
+  const browser = await puppeteer.launch(
+  {
+    headless: false,
+    defaultViewport: {width: 1920, height: 1080},
+  }
+  );
+  try {
+    
+    if(!process.env.PRODOUANE_USER){
+      await browser.close();
+      throw "Initialisez la variable d'environnement PRODOUANE_USER avec le login";
+    }
+    if(!process.env.PRODOUANE_PASS){
+      await browser.close();
+      throw "Initialisez la variable d'environnement PRODOUANE_PASS avec le mot de passe";
+    }
+    if(!process.env.CVI){
+      await browser.close();
+      throw "Initialisez la variable d'environnement CVI avec le numéro de CVI";
+    }
+    
+    console.log("===================");
+    
+    const page = await browser.newPage();
+    const baseURL = 'https://www.douane.gouv.fr'
+    
+    await page.goto(baseURL+"/mon-espace-personnel");
+    
+    console.log("Home page: OK");
+    console.log("===================");
+
+    await page.click('#loginIdentifiant');    
+    await page.waitForSelector('#loginIdentifiant');
+    
+    console.log("Login page: OK");
+    console.log("===================");
+    
+    await page.type('#loginIdentifiant', process.env.PRODOUANE_USER); 
+    await page.type('#loginMotdepasse', process.env.PRODOUANE_PASS);
+    await page.keyboard.press('Enter');
+    
+    console.log("Login: OK");
+    console.log("===================");
+    
+    await page.waitForSelector('#timer');
+    await page.click('button.positive');
+
+    console.log("Click sur CONTINUER OK");
+    console.log("===================");
+
+    await page.goto(baseURL+"/service-en-ligne/redirection/PORTAIL_VITI");
+    
+    console.log("Redirection OK");
+    console.log("===================");
+    
+    await page.waitForTimeout(5000);
+    
+    await page.click("input[value='Fiche de compte']");
+        
+    console.log("Click sur Fiche de compte OK");
+    console.log("===================");
+    
+    await page.waitForTimeout(1000);
+    await page.$$("#formFdc\\:inputNumeroCvi");
+
+    await page.click("#formFdc\\:inputNumeroCvi");    
+    await page.type('#formFdc\\:inputNumeroCvi', process.env.CVI);    
+    
+    await page.click('input[value="Rechercher"]');
+    
+    console.log("Input CVI OK");
+    console.log("===================");
+    
+    await page.waitForSelector("#formFdc\\:dttListeEvvOA\\:0\\:j_idt268");
+    await page.click("#formFdc\\:dttListeEvvOA\\:0\\:j_idt268");    
+    
+    console.log("Click sur l'oeil OK");
+    console.log("===================");
+
+    await page.waitForTimeout(2000);
+
+    const html = await page.content();
+    fs.writeFileSync(process.env.CVI+"_coordonnees_"+".html","<?xml version='1.0' encoding='UTF-8' ?>"+html);
+  	
+    console.log("Enregistre la page HTML des coordonnées de l'opérateur OK");
+    console.log("===================");
+    
+    
+    await page.click('a[href="#formFdcConsultation:j_idt195:j_idt480"]'); 
+    
+    console.log("Click sur Mon parcellaire planté OK");
+    console.log("===================");
+
+    await page.waitForSelector('#formFdcConsultation\\:j_idt195\\:pnlDttListeSpcvPlante ');       
+    fs.writeFileSync(process.env.CVI+"_parcellaire_plante_"+".html",await page.content());
+
+    console.log("Enregistre la page HTML des parcellaire plante de l'opérateur OK");
+    console.log("===================");
+
+    await page.click('#formFdcConsultation\\:j_idt193'); 
+    
+    console.log("Click sur première imprimante OK");
+    console.log("===================");
+    
+    client = await page.target().createCDPSession()
+    await client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: '',
+    });
+
+    await page.waitForTimeout(2000);
+    await page.click('#releveForm\\:j_idt80');
+    await page.waitForTimeout(10000);
+
+    console.log("Click sur deuxième imprimante OK");
+    console.log("===================");
+    
+    await browser.close();
+
+    console.log("FINI POUR LE CVI "+process.env.CVI);  
+    console.log("===================");
+
+    }catch (e) {
+				console.log("");
+				console.log('FAILED !!');
+				console.log(e);
+			}
+		})();
