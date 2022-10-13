bf38ec96c70dfc0b27b8fcbdd8f793aae8ce45fa
Tangui Morlier <tangui@tangui.eu.org>
2022-10-13 14:39:04 +0200

    Debug plus simple

--- puppeteer_scrapping/scriptGetHTMLSAndPDFByCVI.js
+++ puppeteer_scrapping/scriptGetHTMLSAndPDFByCVI.js
@@ -5,7 +5,7 @@ const fs = require('fs');
   
   const browser = await puppeteer.launch(
   {
-    headless: true,  //mettre à false pour debug
+    headless: !(process.env.DEBUG_WITH_BROWSER),  //mettre à false pour debug
     defaultViewport: {width: 1920, height: 1080},
     ignoreDefaultArgs: ['--disable-extensions'],
     args: ['--no-sandbox', '--disable-setuid-sandbox'], 
@@ -25,7 +25,7 @@ const fs = require('fs');
       await browser.close();
       throw "Initialisez la variable d'environnement CVI avec le numéro de CVI";
     }
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("===================");
     }
     const page = await browser.newPage();
@@ -33,14 +33,14 @@ const fs = require('fs');
     
     await page.goto(baseURL+"/mon-espace-personnel");
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Home page: OK");
       console.log("===================");
     }
     await page.click('#loginIdentifiant');    
     await page.waitForSelector('#loginIdentifiant');
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Login page: OK");
       console.log("===================");
     }
@@ -49,7 +49,7 @@ const fs = require('fs');
     await page.type('#loginMotdepasse', process.env.PRODOUANE_PASS);
     await page.keyboard.press('Enter');
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Login: OK");
       console.log("===================");
     }
@@ -57,14 +57,14 @@ const fs = require('fs');
     await page.waitForSelector('#timer');
     await page.click('button.positive');
 
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur CONTINUER OK");
       console.log("===================");
     }
 
     await page.goto(baseURL+"/service-en-ligne/redirection/PORTAIL_VITI");
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Redirection OK");
       console.log("===================");
     }
@@ -72,7 +72,7 @@ const fs = require('fs');
     
     await page.click("input[value='Fiche de compte']");
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur Fiche de compte OK");
       console.log("===================");
     }
@@ -85,7 +85,7 @@ const fs = require('fs');
     
     await page.click('input[value="Rechercher"]');
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Input CVI OK");
       console.log("===================");
     }
@@ -93,7 +93,7 @@ const fs = require('fs');
     await page.waitForSelector("#formFdc\\:dttListeEvvOA\\:0\\:j_idt268");
     await page.click("#formFdc\\:dttListeEvvOA\\:0\\:j_idt268");    
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur l'oeil OK");
       console.log("===================");
     }
@@ -103,14 +103,14 @@ const fs = require('fs');
     const html = await page.content();
     fs.writeFileSync("documents/parcellaire-"+process.env.CVI+"-accueil.html","<?xml version='1.0' encoding='UTF-8' ?>"+html);
   	
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Enregistre la page HTML des coordonnées de l'opérateur OK");
       console.log("===================");
     }
         
     await page.click('a[href="#formFdcConsultation:j_idt195:j_idt480"]'); 
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur Mon parcellaire planté OK");
       console.log("===================");
     }
@@ -118,14 +118,14 @@ const fs = require('fs');
     await page.waitForSelector('#formFdcConsultation\\:j_idt195\\:pnlDttListeSpcvPlante ');       
     fs.writeFileSync("documents/parcellaire-"+process.env.CVI+"-parcellaire.html",await page.content());
 
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Enregistre la page HTML des parcellaire plante de l'opérateur OK");
       console.log("===================");
     }
     
     await page.click('#formFdcConsultation\\:j_idt193'); 
     
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur première imprimante OK");
       console.log("===================");
     }
@@ -140,14 +140,14 @@ const fs = require('fs');
     await page.click('#releveForm\\:j_idt80');
     await page.waitForTimeout(10000);
 
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("Click sur deuxième imprimante OK");
       console.log("===================");
     }
     
     fs.rename('documents/Fiches_de_compte_'+process.env.CVI+'.pdf', 'documents/parcellaire-'+process.env.CVI+'-parcellaire.pdf', (err) => {
         if (err) throw err;
-        if(process.env.DEBUG == "true"){
+        if(process.env.DEBUG){
           console.log('Rename OK!');
           console.log("===================");
         }
@@ -155,7 +155,7 @@ const fs = require('fs');
     
     await browser.close();
 
-    if(process.env.DEBUG == "true"){
+    if(process.env.DEBUG){
       console.log("FINI POUR LE CVI "+process.env.CVI);  
       console.log("===================");
     }
