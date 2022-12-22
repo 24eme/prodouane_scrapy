0b48b2620ed7129e46fd88e056bebd9d98c1c164
Jb Lm <lemetayer.jb@gmail.com>
2022-12-22 15:27:04 +0100

    clique sur la declaration de stock en consultation par installation

--- puppeteer_scrapping/prodouane_stock.js
+++ puppeteer_scrapping/prodouane_stock.js
@@ -5,51 +5,61 @@ const fs = require('fs');
 (async () => {
   try {
     page = await prodouane.openpage_and_login();
-    
+
     await page.click("input[value='Stock']");
     await page.waitForSelector('#formDeclaration');
-    
+
     console.log("Click sur Stock OK");
     console.log("===================");
-  
+
     if(!process.env.CVI){
       if(process.env.DEBUG){
         console.log("LISTER TOUS LES CVIS");
         console.log("===================");
       }
-       
+
        //LISTER TOUS LES CVIS EN SORTIE STANDARD
      await prodouane.close();
      return;
     }
-    
-    
+
+
     await page.click("#formFiltre\\:inputNumeroCvi");
-    await page.type('#formFiltre\\:inputNumeroCvi', process.env.CVI);    
-    
+    await page.type('#formFiltre\\:inputNumeroCvi', process.env.CVI);
+
     await page.click('input[value="Filtrer par EVV"]');
-    
+
     console.log("Input CVI OK");
     console.log("===================");
-    
-    //verifier que le CVI existe 
-    var hasError = (await page.$("#erreur\\:j_idt113")) || false;
-    
-    console.log(hasError);
-    // 
+
+
+    await page.waitForSelector("#formDeclaration\\:listeDeclaration\\:j_idt140");
+    var result = await page.$("#formDeclaration\\:listeDeclaration\\:0\\:j_idt172");
+
+    if (result === null) {
+      console.log('Aucun résultat pour le CVI donné');
+      await prodouane.close();
+    }
+
+    await page.click("#formDeclaration\\:listeDeclaration\\:0\\:j_idt172 a");
+
+    console.log("Click sur Consulter par installation");
+    console.log("===================");
+
+    //
     // if(hasError){
     //   console.log("");
     //   console.log('FAILED !! IL Y A UNE ERREUR CVI inconnu');
     //   await prodouane.close();
     //   return;
-    // }    
-    // 
-                     
+    // }
+    //
+
   }catch (e) {
     console.log("");
     console.log('FAILED !!');
     console.log(e);
     await prodouane.close();
   }
-  
+
 })();
