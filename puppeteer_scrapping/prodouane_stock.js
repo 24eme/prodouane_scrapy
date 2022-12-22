12e700f8ae54e8d3b848f81867187b5023fae2a0
Honorine-MC <honorine.mariechristy@gmail.com>
2022-12-22 16:01:28 +0100

    téléchargement de la page

--- puppeteer_scrapping/prodouane_stock.js
+++ puppeteer_scrapping/prodouane_stock.js
@@ -46,15 +46,41 @@ const fs = require('fs');
     console.log("Click sur Consulter par installation");
     console.log("===================");
 
-    //
-    // if(hasError){
-    //   console.log("");
-    //   console.log('FAILED !! IL Y A UNE ERREUR CVI inconnu');
-    //   await prodouane.close();
-    //   return;
-    // }
-    //
+    await page.waitForSelector("#j_idt172\\:0");
+    fs.writeFileSync("documents/ds-"+process.env.CVI+".html",await page.content());
+    
+    if(process.env.DEBUG){
+      console.log("Enregistre la page HTML des stock de l'opérateur OK");
+      console.log("===================");
+    }
+    
+        
+    await page.click('#j_idt172\\:j_idt178 a');  
+    console.log("Click sur pdf");
+    console.log("===================");
+    
 
+    var csv_filename = '';
+    await page.waitForResponse((response) => {
+        if (response.status() === 200) {
+            csv_filename = response.headers()['content-disposition'];
+            csv_filename = csv_filename.replace('attachment;filename=', '');
+            console.log(csv_filename);
+            if (csv_filename.match('pdf')) {
+                return true;
+            }
+        }
+        return false;
+    });
+    
+    
+    // session_id = csv_filename.match('DeclarationStock_([^_]+)_([^_]+)_([^\.]+)\.pdf');  DeclarationStock_6900703220_2122_RecapitulatifInstallation.pdf
+    // await page.waitForTimeout(100);
+    // await fs.rename('documents/'+csv_filename, 'documents/production-'+process.env.PRODOUANE_ANNEE+'-'+session_id[1]+'.csv', (err) => {if (err) throw err;});
+    
+    
+    // await page.click('#j_idt172\\:j_idt178 a');
+    
   }catch (e) {
     console.log("");
     console.log('FAILED !!');
