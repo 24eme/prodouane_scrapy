057590b37cf1c45ab2e06dad0bab64da75b70ee9
Honorine-MC <honorine.mariechristy@gmail.com>
2022-12-23 12:34:46 +0100

    récupération et renommage de tous les fichiers

--- puppeteer_scrapping/prodouane_stock.js
+++ puppeteer_scrapping/prodouane_stock.js
@@ -29,8 +29,10 @@ const fs = require('fs');
 
     await page.click('input[value="Filtrer par EVV"]');
 
-    console.log("Input CVI OK");
-    console.log("===================");
+    if(process.env.DEBUG){
+      console.log("Input CVI OK");
+      console.log("===================");
+    }
 
 
     await page.waitForSelector("#formDeclaration\\:listeDeclaration\\:j_idt140");
@@ -41,10 +43,16 @@ const fs = require('fs');
       await prodouane.close();
     }
 
+    await page.waitForSelector("#formDeclaration\\:listeDeclaration\\:0\\:j_idt172");
+
+    await page.waitForTimeout(100);
+
     await page.click("#formDeclaration\\:listeDeclaration\\:0\\:j_idt172 a");
 
-    console.log("Click sur Consulter par installation");
-    console.log("===================");
+    if(process.env.DEBUG){
+      console.log("Click sur Consulter par installation");
+      console.log("===================");
+    }
 
     await page.waitForSelector("#j_idt172\\:0");
     fs.writeFileSync("documents/ds-"+process.env.CVI+".html",await page.content());
@@ -62,23 +70,16 @@ const fs = require('fs');
     downloadPath: "documents",
     });
 
-    // await page.$$('#j_idt172\\:j_idt178 a(title="Générer un fichier pdf pour impression")');
-
-    // console.log(liens);
-    // await page.click('a(title="Générer un fichier pdf pour impression")');
-
-
-
     // Download du PDF
 
     liens = await page.$$('#j_idt172\\:j_idt178 a');
 
-    //await page.click('#j_idt172\\:j_idt178:nth-child(1)');
-
     await liens[0].click();
 
-    console.log("Click sur pdf");
-    console.log("===================");
+    if(process.env.DEBUG){
+      console.log("Click sur pdf");
+      console.log("===================");
+    }
 
     var pdf_filename = '';
 
@@ -100,48 +101,44 @@ const fs = require('fs');
     pdf_newfilename = pdf_newfilename.replace('_', '-');
     await fs.rename('documents/'+pdf_filename, 'documents/'+pdf_newfilename, (err) => {if (err) throw err;});
 
+    if(process.env.DEBUG){
+      console.log("Renommage de "+'documents/'+pdf_filename+"en"+'documents/'+pdf_newfilename+" OK");
+      console.log("===================");
+    }
+
     // Download du XLS
-    //await page.click("a[title='Générer un tableur Excel ou Calc']");
 
     await liens[1].click();
 
-    console.log("Click sur xls");
-    console.log("===================");
-
+    if(process.env.DEBUG){
+      console.log("Click sur xls");
+      console.log("===================");
+    }
+    
     var xls_filename = '';
     await page.waitForResponse((response) => {
         if (response.status() === 200) {
             xls_filename = response.headers()['content-disposition'];
             xls_filename = xls_filename.replace('attachment; filename=', '');
-            console.log(xls_filename);
             if (xls_filename.match(/.xls$/)) {
-                console.log('TRUE');
                 return true;
             }
         }
-        console.log('FALSE');
         return false;
     });
-
-    await page.waitForTimeout(100);
 
     xls_newfilename = xls_filename.replace('DeclarationStock', 'ds');
     xls_newfilename = xls_newfilename.replace('_RecapitulatifInstallation', '');
     xls_newfilename = xls_newfilename.replace('_', '-');
+
+    await page.waitForTimeout(5000); //attendre le téléchargement complet et non du fichier temporaire de chromium .crdownload
+
     await fs.rename('documents/'+xls_filename, 'documents/'+xls_newfilename, (err) => {if (err) throw err;});
 
-return;
-
-    await page.click('#buttonLogin');
-    await page.waitForSelector('.logout');
-    await page.click('.logout');
-
-    await page.waitForSelector('.erreur-authentification')
     if(process.env.DEBUG){
-      console.log("Déconnexion OK");
+      console.log("Renommage de "+'documents/'+xls_filename+"en"+'documents/'+xls_newfilename+" OK");
       console.log("===================");
     }
-
     await prodouane.close();
 
   }catch (e) {
