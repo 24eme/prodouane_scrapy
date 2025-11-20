ec33e622a80b26b594cd111c84670daaef7f775c
Gabriel Poma <gpoma@24eme.fr>
2025-11-20 11:54:34 +0100

    rhone: on check si y'a un cépage dans la transfo en csv

--- posttraitement/parcellaire_html_to_csv.py
+++ posttraitement/parcellaire_html_to_csv.py
@@ -157,7 +157,8 @@ with open(directory + filename % 'accueil', 'rb') as html_file:
                 parcellaire['Produit'] = ""
                 parcellaire['Superficie cadastrale'] = ""
 
-            parcellaire['Cepage'] = infos_parcelles[4].strip()
+            if infos_parcelles[4]:
+                parcellaire['Cepage'] = infos_parcelles[4].strip()
 
             parcellaire['Superficie'] = transform_superficie(infos_parcelles[5])
 
